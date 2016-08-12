52d0beea130e6fb746edea5a78d5f7ffcdcf6ba4
peterirungu <peterirungu16@gmail.com>
2016-08-12 13:47:10 +0300

    created and styled the form

---
+++ login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title></title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS 
+    <link href="css/sb-admin.css" rel="stylesheet">
+-->
+
+    <!-- Custom CSS -->
+    <link href="css/login-form.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+</head>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 col-md-4 col-md-offset-4">
+            <h1 class="text-center login-title">Login to stlech.</h1>
+            <div class="account-wall">
+                <img class="profile-img" src="./images/user-icon.jpeg" alt="">
+                <form class="form-signin">
+                <input type="text" class="form-control" placeholder="Username" required autofocus>
+                <input type="password" class="form-control" placeholder="Password" required>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                <label class="checkbox pull-left">
+                    <input type="checkbox" value="remember-me">
+                    Remember me
+                </label>
+                <a href="#" class="pull-right need-help">Forgot password!!!</a><span class="clearfix"></span>
+                </form>
+            </div>            
+        </div>
+    </div>
+</div>
+</body>
+</html>
